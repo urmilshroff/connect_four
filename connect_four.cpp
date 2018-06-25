72aa742dc613f47e3ed5de816cc635281652da88
urmilshroff <urmilshroff@gmail.com>
2018-06-25 20:32:09 +0530

    Updated a lot of things

--- connect_four.cpp
+++ connect_four.cpp
@@ -4,70 +4,68 @@ using namespace std;
 class Board
 {
 public:
-    
-int player,rowPos,colPos,rowPos1,colPos1,rowPos0,colPos0,board[6][7]; //board of 6 rows and 7 columns
-string p1,p2;
+int rowPos,colPos;
 
 Board()
 {
+    string p1,p2,board[6][7]; //board of 6 rows and 7 columns
+    
     cout<<"Let's play!"<<endl; //2 players for now (TODO: AI)
     cout<<"Enter player 1 name:"<<endl;
     cin>>p1;
     cout<<"Enter player 2 name:"<<endl;
     cin>>p2;
-
-    display(NULL,NULL,NULL,NULL,true); //initially the board is empty
-    updater();
-}
-
-
-int updater()
-{
-    return 0;
-}
-
-
-int display(int rowPos1,int colPos1,int rowPos0,int colPos0,bool isEmpty)
-{
-    //cout<<rowPos1<<colPos1<<rowPos0<<colPos0;
     
-    cout<<"Current board looks like this:"<<endl;
     for(rowPos=0;rowPos<6;rowPos++)
     {
         for(colPos=0;colPos<7;colPos++)
         {
-            if (rowPos==rowPos1 && colPos==colPos1 && isEmpty==false)
-            {
-                cout<<"1 ";
-            }
-            
-            else if(rowPos==rowPos0 && colPos==colPos0 && isEmpty==false)
-            {
-                cout<<"0 ";
-            }
-            
-            else
-            {
-                cout<<"_ ";
-            }
+            board[rowPos][colPos]="_";
         }
-        cout<<endl;
-    }
-    return 0;
+    } //initializes empty board
+
+    display(board[6][7]); //initially the board is empty
+    //updater(p1,p2,board);
 }
 
+
+void updater(string p1,string p2,string board) //the main logic lies here
+{
+    int colPosX=playerChoice(p1);
+    //board[row][colPos1];
+    
+    
+}
 
 
 int playerChoice(string player)
 {
-    cout<<"Enter the column you wish to play in, "<<player<<":"<<endl;
+    int colPos;
+    cout<<"It's your turn, "<<player<<":"<<endl;
     cin>>colPos;
-    return colPos;
-}    
+    return colPos-1; //because array position counting starts from 0
+} 
+
+
+
+void display(string board)
+{
+
+    cout<<"Current board looks like this:"<<endl;
+
+    for(rowPos=0;rowPos<6;rowPos++)
+    {
+        for(colPos=0;colPos<7;colPos++)
+        {
+            cout<<board[rowPos][colPos];
+        }
+        cout<<endl;
+    }
     
+    cout<<"1 2 3 4 5 6 7"<<endl; //positional indicators for each column
+}
 
 };
-
 
 
 int main()
