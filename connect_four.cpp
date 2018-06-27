10d98a85b35ef867da311681444ad6600884d7c6
urmilshroff <urmilshroff@gmail.com>
2018-06-28 02:49:06 +0530

    Made rowPos a bit smarter in updater()

--- connect_four.cpp
+++ connect_four.cpp
@@ -33,26 +33,26 @@ Board()
 
 void updater(string board[6][7],string playerName[2]) //updates board with new moves
 {
-    int i,p=0,rowPos,colPos,winner;
+    int i,p=0,rowPos[7],colPos,winner;
     bool someWon; //by default it is false, nobody has won yet
     
     while(someWon!=true) //when somebody wins, loop breaks out
     {
         colPos=playerChoice(playerName[p]); //player's choice goes into board
         
-        if(board[5][colPos]=="_ ")
+        if(board[5][colPos]=="_ ") //if no insert in that column yet
         {
-            rowPos=5;
+            rowPos[colPos]=5;
         }
         
-        while(board[rowPos][colPos]!="_ ") //if someone has already played in that position
+        while(board[rowPos[colPos]][colPos]!="_ ") //if someone has already played in that position
         {
-            rowPos--; //keeps going up up up!
+            rowPos[colPos]--; //keeps going up up up!
         }
         
         if(p==0) //if player 1 then use X
         {
-            board[rowPos][colPos]="X ";
+            board[rowPos[colPos]][colPos]="X ";
             someWon=checker(board);
             if(someWon!=true)
             {
@@ -62,7 +62,7 @@ void updater(string board[6][7],string playerName[2]) //updates board with new m
         
         else if(p==1) //if player 2 then use O
         {    
-            board[rowPos][colPos]="O ";
+            board[rowPos[colPos]][colPos]="O ";
             someWon=checker(board);
             if(someWon!=true)
             {
@@ -88,23 +88,9 @@ bool checker(string board[6][7]) //the main logic lies here
 int playerChoice(string player) //try renaming parameter to playerName later
 {
     int colPos;
-    bool validInput;
     
     cout<<"It's your turn, "<<player<<"! Which column do you want to play in?"<<endl;
-    while(validInput!=true) //when input is valid it breaks out
-    {
-        cin>>colPos;
-        
-        if(colPos<1 || colPos>7)
-        {
-            cout<<"Sorry, please enter a valid column number between 1 and 7:"<<endl;
-        }
-        else
-        {
-            validInput=true;
-        }
-    }
-    
+    cin>>colPos;
     return colPos-1; //because array position counting starts from 0
 } 
 
